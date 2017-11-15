c3f3fa02b6e510b1c2e9cc203461a3d5ef2d02a3
ywj <ywj@ywjdeMacBook-Pro.local>
2017-11-15 17:36:03 +0800

    feat: 完成实验分类任务

--- __init__.py
+++ __init__.py
@@ -1,77 +1,214 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import glob
 import os
+import shutil
 import pandas as pd  # 处理csv文件
 import numpy as np
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
 
-dir_name = 'data'
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
+
+train_dir_name = 'train'
+validation_dir_name = 'validation'
 superpixels_name = 'ISIC_*_superpixels.png'
-csv_name = 'ISIC-2017_Training_Part3_GroundTruth.csv'
+train_csv_name = 'ISIC-2017_Training_Part3_GroundTruth.csv'
+validation_csv_name = 'ISIC-2017_Validation_Part3_GroundTruth.csv'
 data_list = {}
-data_type = ['Melanoma', 'Nevus', 'seborrheic_keratosis']
-validation_percentage = 10
-test_percentage = 10
+data_type = ['melanoma', 'nevus', 'seborrheic_keratosis']
+nb_classes = len(data_type)
+nb_epoch = 3
+batch_size = 50
 
+IM_WIDTH = 299
+IM_HEIGHT = 299
+FC_SIZE = 1024
 
 # 删除超像素文件
 def delete_superpixels_from_data():
-    dir_path = os.path.join(os.curdir, dir_name)
-    file_path = os.path.join(dir_path, superpixels_name)
-    if os.path.exists(dir_path):
+    train_dir_path = os.path.join(os.curdir, train_dir_name)
+    validation_dir_path = os.path.join(os.curdir, validation_dir_name)
+
+    file_path = os.path.join(train_dir_path, superpixels_name)
+    if os.path.exists(train_dir_path):
         file_list = glob.glob(file_path)
         for path in file_list:
             os.remove(path)
-        print('Removed superpixel files from data directory.')
-    else:
-        print("Directory name is wrong.")
+        print 'Removed superpixel files from train directory.'
+
+    file_path = os.path.join(validation_dir_path, superpixels_name)
+    if os.path.exists(validation_dir_path):
+        file_list = glob.glob(file_path)
+        for path in file_list:
+            os.remove(path)
+        print 'Removed superpixel files from validation directory.'
 
 
-def create_data_list():
+def create_train_dir():
     # 初始化图像数据字典
-    for type_name in data_type:
-        data_list[type_name] = {
-            'images': [],
-            'validation': [],
-            'test': []
-        }
+    # for type_name in data_type:
+    #     data_list[type_name] = {
+    #         'images': [],
+    #         'validation': [],
+    #         'test': []
+    #     }
 
     # 获取ground_truth数据的路径
-    dir_path = os.path.join(os.curdir, dir_name)
-    file_path = os.path.join(dir_path, csv_name)
+    dir_path = os.path.join(os.curdir, train_dir_name)
+    file_path = os.path.join(dir_path, train_csv_name)
 
     # 构造数据字典
     if os.path.exists(file_path):
         csv_info = pd.read_csv(file_path)
         for index, row in csv_info.iterrows():
-            chance = np.random.randint(100)
             file_name = os.path.join(dir_path, row[0] + '.jpg')
-            # 验证集
-            if chance < validation_percentage:
-                if row[1] == 1.0:
-                    data_list[data_type[0]]['validation'].append(file_name)
-                elif row[2] == 1.0:
-                    data_list[data_type[2]]['validation'].append(file_name)
-                else:
-                    data_list[data_type[1]]['validation'].append(file_name)
-            # 测试集
-            elif chance < (test_percentage + validation_percentage):
-                if row[1] == 1.0:
-                    data_list[data_type[0]]['test'].append(file_name)
-                elif row[2] == 1.0:
-                    data_list[data_type[2]]['test'].append(file_name)
-                else:
-                    data_list[data_type[1]]['test'].append(file_name)
             # 训练集
+            if row[1] == 1.0:
+                des_file_name = os.path.join(dir_path, data_type[0], row[0] + '.jpg')
+                des_dir_name = os.path.join(dir_path, data_type[0])
+            elif row[2] == 1.0:
+                des_file_name = os.path.join(dir_path, data_type[2], row[0] + '.jpg')
+                des_dir_name = os.path.join(dir_path, data_type[2])
             else:
-                if row[1] == 1.0:
-                    data_list[data_type[0]]['images'].append(file_name)
-                elif row[2] == 1.0:
-                    data_list[data_type[2]]['images'].append(file_name)
-                else:
-                    data_list[data_type[1]]['images'].append(file_name)
-        print('Create data dict success.')
+                des_file_name = os.path.join(dir_path, data_type[1], row[0] + '.jpg')
+                des_dir_name = os.path.join(dir_path, data_type[1])
+
+            if not os.path.exists(des_dir_name):
+                os.mkdir(des_dir_name)
+            shutil.move(file_name, des_file_name)
+        print 'Create training category dir success.'
     else:
-        print("CSV file path is wrong.")
+        raise EnvironmentError('Train CSV file path is wrong!')
+
+
+def create_validation_dir():
+    # 初始化图像数据字典
+    # for type_name in data_type:
+    #     data_list[type_name] = {
+    #         'images': [],
+    #         'validation': [],
+    #         'test': []
+    #     }
+
+    # 获取ground_truth数据的路径
+    dir_path = os.path.join(os.curdir, validation_dir_name)
+    file_path = os.path.join(dir_path, validation_csv_name)
+
+    # 构造数据字典
+    if os.path.exists(file_path):
+        csv_info = pd.read_csv(file_path)
+        for index, row in csv_info.iterrows():
+            file_name = os.path.join(dir_path, row[0] + '.jpg')
+            # 训练集
+            if row[1] == 1.0:
+                des_file_name = os.path.join(dir_path, data_type[0], row[0] + '.jpg')
+                des_dir_name = os.path.join(dir_path, data_type[0])
+            elif row[2] == 1.0:
+                des_file_name = os.path.join(dir_path, data_type[2], row[0] + '.jpg')
+                des_dir_name = os.path.join(dir_path, data_type[2])
+            else:
+                des_file_name = os.path.join(dir_path, data_type[1], row[0] + '.jpg')
+                des_dir_name = os.path.join(dir_path, data_type[1])
+
+            if not os.path.exists(des_dir_name):
+                os.mkdir(des_dir_name)
+            shutil.move(file_name, des_file_name)
+        print 'Create validation category dir success.'
+    else:
+        raise EnvironmentError('Validation CSV file path is wrong!')
+
+
+def get_nb_files(directory):
+    """获取样本个数"""
+    if not os.path.exists(directory):
+        return 0
+    cnt = 0
+    for r, dirs, files in os.walk(directory):
+        for dr in dirs:
+            cnt += len(glob.glob(os.path.join(r, dr + "/*")))
+    return cnt
+
+
+# 添加新层
+def add_new_last_layer(base_model, nb_classes):
+    """
+    添加最后的层
+    输入
+    base_model和分类数量
+    输出
+    新的keras的model
+    """
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(FC_SIZE, activation='relu')(x)
+    predictions = Dense(nb_classes, activation='softmax')(x)
+    model = Model(input=base_model.input, output=predictions)
+    return model
+
+
+# 冻上base_model所有层，这样就可以正确获得bottleneck特征
+def setup_to_transfer_learn(model, base_model):
+    """Freeze all layers and compile the model"""
+    for layer in base_model.layers:
+        layer.trainable = False
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 if __name__ == '__main__':
-    delete_superpixels_from_data()
-    create_data_list()
+    # 数据预处理
+    # delete_superpixels_from_data()
+    # create_train_dir()
+    # create_validation_dir()
+    train_dir = os.path.join(os.path.curdir, 'train')
+    val_dir = os.path.join(os.path.curdir, 'validation')
+
+    nb_train_samples = get_nb_files(train_dir)
+    nb_classes = len(glob.glob(train_dir + '/*'))
+    nb_val_samples = get_nb_files(val_dir)
+    nb_epoch = int(nb_epoch)
+    batch_size = int(batch_size)
+
+    train_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
+        rotation_range=30,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
+    test_datagen = ImageDataGenerator(
+        preprocessing_function=preprocess_input,
+        rotation_range=30,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True
+    )
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(IM_WIDTH, IM_HEIGHT),
+        batch_size=batch_size, class_mode='categorical')
+    validation_generator = test_datagen.flow_from_directory(
+        val_dir,
+        target_size=(IM_WIDTH, IM_HEIGHT),
+        batch_size=batch_size, class_mode='categorical')
+
+    # 定义网络框架
+    base_model = InceptionV3(weights='imagenet', include_top=False)  # 预先要下载no_top模型
+    model = add_new_last_layer(base_model, nb_classes)  # 从基本no_top模型上添加新层
+    setup_to_transfer_learn(model, base_model)  # 冻结base_model所有层
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    history_tl = model.fit_generator(
+        train_generator,
+        nb_epoch=nb_epoch,
+        samples_per_epoch=nb_train_samples,
+        validation_data=validation_generator,
+        nb_val_samples=nb_val_samples,
+        class_weight='auto')
